9f008f9f59fdb5b80c75fc25aaf6458273579628
Julio Mejía <julio.mejia@javeriana.edu.co>
2021-03-23 19:03:54 -0500

    Initial Structure

---
+++ src/main/java/com/webos/turistar/TuristarApplication.java
@@ -0,0 +1,13 @@
+package com.webos.turistar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristarApplication.class, args);
+	}
+
+}
