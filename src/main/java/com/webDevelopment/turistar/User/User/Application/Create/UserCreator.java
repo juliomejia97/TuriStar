3dc10a292c74fd6d8eb6bd41bb456772d82ca4b0
AMendoza04 <daniel.mendo04@gmail.com>
2021-05-04 15:27:27 -0500

    minor changes

--- src/main/java/com/webDevelopment/turistar/User/User/Application/Create/UserCreator.java
+++ src/main/java/com/webDevelopment/turistar/User/User/Application/Create/UserCreator.java
@@ -1,47 +1,55 @@
 package com.webDevelopment.turistar.User.User.Application.Create;
 
 import com.webDevelopment.turistar.Shared.Domain.User.UserId;
+import com.webDevelopment.turistar.User.User.Domain.Exceptions.UserAlreadyExists;
 import com.webDevelopment.turistar.User.User.Domain.Exceptions.UserNotExist;
 import com.webDevelopment.turistar.User.User.Domain.Ports.UserDomainFinder;
+import com.webDevelopment.turistar.User.User.Domain.Ports.UserRepository;
 import com.webDevelopment.turistar.User.User.Domain.Ports.UserValidateWords;
+import com.webDevelopment.turistar.User.User.Domain.Ports.ValidateWordService;
 import com.webDevelopment.turistar.User.User.Domain.User;
 import com.webDevelopment.turistar.User.User.Domain.ValueObjects.UserEmail;
 import com.webDevelopment.turistar.User.User.Domain.ValueObjects.UserLastName;
 import com.webDevelopment.turistar.User.User.Domain.ValueObjects.UserName;
 import com.webDevelopment.turistar.User.User.Domain.ValueObjects.UserPassword;
-import com.webDevelopment.turistar.User.User.Infrastructure.BadWordsService;
-import com.webDevelopment.turistar.User.User.Infrastructure.InMemoryUserRepository;
+
+import java.util.Optional;
 
 
 public class UserCreator {
-    private InMemoryUserRepository inMemoryUserRepository;
-    private BadWordsService badWordsService;
+    private UserRepository repository;
+    private ValidateWordService service;
     private UserValidateWords validator;
     private UserDomainFinder finder;
 
-    public UserCreator(InMemoryUserRepository inMemoryUserRepository, BadWordsService badWordsService) {
-        this.inMemoryUserRepository = inMemoryUserRepository;
-        this.badWordsService = badWordsService;
-        this.validator = new UserValidateWords(badWordsService);
-        this.finder = new UserDomainFinder(inMemoryUserRepository);
+    public UserCreator(UserRepository repository, ValidateWordService service) {
+        this.repository = repository;
+        this.service = service;
+        this.validator = new UserValidateWords(service);
+        this.finder = new UserDomainFinder(repository);
     }
 
     public void execute(String userId, String userFirstName, String userLastName, String userEmail, String userPassword)
     {
         this.validate(userId);
         validator.execute(new UserEmail(userEmail).value());
-
-
         User user = new User(new UserId(userId), new UserName(userFirstName), new UserLastName(userLastName) , new UserEmail(userEmail),new UserPassword(userPassword));
-        inMemoryUserRepository.save(user);
+        repository.save(user);
     }
 
     private void validate(String UserId)
     {
         try
         {
-            this.finder.execute(UserId);
+
+            Optional<User> user = this.finder.execute(UserId);
+            if(!user.isEmpty())
+            {
+                throw new UserAlreadyExists("User with id: " + UserId + " already exist");
+            }
         }
-        catch (UserNotExist exception) { }
+        catch (UserNotExist exception) {
+
+        }
     }
 }
